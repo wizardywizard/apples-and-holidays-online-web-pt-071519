46be089b80812c81d268ea340962400e032f69ab
David Little <littledavid175@gmail.com>
2019-07-21 18:54:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
      # binding.pry
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+        end
       end
     end
   end
